748873aefd3440d6873364d8f121939cdd43efee
Richard Montgomery <rikermonty@gmail.com>
2017-10-18 20:47:12 -0700

    Added a properly working question 6 and have started Question 7

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
 'use strict';
+//Beginning of ASSIGNMENT TWO
 /*
-alert('Do you want to play a game? \nThey call it "How well do you know Richard?" \n\nThe only site that knows YOU CARE!');
-alert('You will be given 5 questions about me to determine whether we should friends. \n\n Answer with a "yes" or "y" or with a "no" or "n"');
+//Introduction and Rules to my Guessing Game
+alert('I want you to play a guessing game. \nThey call it "How well do you know Richard?" \n\nThe only site that knows you care!');
+alert('You will be given 5 questions about me.\n\n Answer with a "yes" or "y" or with a "no" or "n"');
 
+//User enters their name
 var user = prompt('Well Player, let\'s start by getting your name:');
 console.log('User\'s name:', user);
 
+alert('Thank you ' + user + '. Let\'s play!');
+
+//Question 1
 var one = prompt('Given the option of mutant abilities, would I want to be able to fly?');
 console.log('Question One Answer:', one);
 
 one = one.toLowerCase();
 
 if(one === 'yes' || one === 'y') {
-  alert('Nah... I have a healthy fear of heights.');
+  alert('Nah... I have a reasonable fear of heights that has preserved my health.');
 } else if(one === 'no' || one === 'n'){
   alert('That\'s right! Flying is for squirrels and fish.');
 };
 
-var two = prompt('Am I tired?');
+//Question 2
+var two = prompt('Am I tired often?');
 console.log('Question Two Answer:', two);
 
 two = two.toLowerCase();
 
 if(two === 'yes' || two === 'y') {
-  alert('Yes! I\'m always tired. I take a car/boat/bus-ride to class and back.');
+  alert('Yes! Very often. I take a car/boat/bus-ride to class and back.');
 } else if(two === 'no' || two === 'n'){
-  alert('That\'s incorrect. I\'m so tired. I survive on micro naps.');
+  alert('That\'s incorrect. I\'m so tired. I survive on 15 second micro naps.');
 };
 
+//Question 3
 var three = prompt('Are lagers my favorite beers?');
 console.log('Question Three Answer:', three);
 
@@ -36,20 +44,22 @@ three = three.toLowerCase();
 if(three === 'yes' || three === 'y') {
   alert('Nope. IPAs are the right PAs.');
 } else if(three === 'no' || three === 'n'){
-  alert('That\'s right! Let\'s go put back a few.');
+  alert('That\'s right! IPAs are the right PAs.');
 };
 
+//Question 4
 var four = prompt('Should they reboot the tv series Firefly?');
 console.log('Question Four Answer:', four);
 
 four = four.toLowerCase();
 
 if(four === 'yes' || four === 'y') {
-  alert('They absolutely should.');
+  alert('They absolutely should. Some of us wonder what\'s taking so long.');
 } else if(four === 'no' || four === 'n'){
-  alert('Honestly, "No" shouldn\'t have even been a choice.');
+  alert('Honestly, the answer "No" shouldn\'t even be a choice.');
 };
 
+//Question 5
 var five = prompt('Is money the root of my happiness?');
 console.log('Question Five Answer:', five);
 
@@ -58,15 +68,18 @@ five = five.toLowerCase();
 if(five === 'yes' || five === 'y') {
   alert('Oh, for sho.');
 } else if(five === 'no' || five === 'n'){
-  alert('I guess it isn\'t everything...');
+  alert('Yea... I guess it isn\'t everything...');
 };
+//End of ASSIGNMENT TWO
 
-*/
+//Beginning of ASSIGNMENT THREE
+//Question 6
 var number;
 var counter = 1;
 
 while(counter < 5){
   number = prompt('About how many IPAs do you think until my body starts to work in reverse?');
+  console.log('Counter:', counter);
   if(isNaN(number) || number === null) {
     alert('Please enter a number.');
   } else if(number < 15) {
@@ -78,17 +91,15 @@ while(counter < 5){
   } else if(number > 15) {
     alert('I\'m probably getting closer to shitting myself at ' + number + ' IPAs.');
     counter++;
-  } else if(number === 15) {
+  } else if(Number(number) === 15) {
+    alert('Nice!');
     break;
   }
 }
+*/
 
-console.log('Counter:', counter);
-
-alert('Nailed it!' ,user);
-
-/*
-var user = prompt('What\'s your name?');
+/*An exercise in not reading thoroughly
+var user = prompt('What\'s your name again?');
 console.log('User\'s name:', user);
 
 alert('So anyways ' + user + '. Soon it will be Halloween. I think it\'s a good idea to make a list of scary things to avoid this year. Thing\'s like Heights, Public Pooping, and Narcissists in Power.');
@@ -106,32 +117,16 @@ for(var i = 0 ; i < list.length ; i++){
 
 alert('The Scary List is Complete!');
 alert(list + '.');
-
-/*
-var wrong = 0;
-var right  = 0;
-
-var food = ['beer', 'pizza', 'sandwiches', 'coffee'];
-var answer = prompt('What foods or drinks preserve my mental health?');
-answer = answer.toLowerCase();
-var flag;
-
-for (var i = 0; i < food.length; i++) {
-  console.log('each iteration:', food[i]);
-  if (answer === food[i]) {
-    alert('High-Five!');
-    flag = true;
-    right = right + 1;
-  } else if(i === 5) {
-    break;
-  }
-}
-
-if (!flag) {
-  alert('Sorry, incorrect');
-  wrong = wrong + 1;
-}
-
-alert(right + ' Correct');
-alert(wrong + ' Wrong');
 */
+
+//var wrong = 0;
+//var correct  = 0;
+var count = 1;
+//var food = ['beer', 'pizza', 'sandwiches', 'mocha'];
+
+while(count < 7) {
+  var answer = prompt('What foods or drinks preserve my mental health?');
+  answer = answer.toLowerCase();
+  console.log('Guess: ', answer);
+  count++;
+}
